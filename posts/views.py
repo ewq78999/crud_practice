0ad01f567eeda650ed96278f2d86ace7c095a404
whitecrow <edin.whitecrow@gmail.com>
2023-08-22 16:18:06 +0900

    first

---
+++ posts/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, 'index.html', context)
+
+def detail(request, id):
+    post = Post.objects.jet(id=id)
+
+    context = {
+        'post' : post,
+    }
+
+    return render(request, 'detail.html', context)
+
+def new(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    post = Post()
+    post.title = title
+    post.content = content
+    post.save()
+
+    return redirect(f'/posts/{post.id}/')
+
+def delete(request, id):
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post' : post,
+    }
+
+    return render(request, 'edit.html', context)
